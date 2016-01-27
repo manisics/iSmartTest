dc22457ed56181297b86756115a8e0eedf5fc195
Mani Murugan <ManiMurugan@manis-mac-mini.local>
2016-01-27 11:48:40 +0530

    New files added in another branch

---
+++ GitUseExample/SampleFile.h
@@ -0,0 +1,13 @@
+//
+//  SampleFile.h
+//  GitUseExample
+//
+//  Created by Mani Murugan on 1/27/16.
+//  Copyright © 2016 Mani Murugan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleFile : NSObject
+
+@end
---
+++ GitUseExample/SampleFile.m
@@ -0,0 +1,13 @@
+//
+//  SampleFile.m
+//  GitUseExample
+//
+//  Created by Mani Murugan on 1/27/16.
+//  Copyright © 2016 Mani Murugan. All rights reserved.
+//
+
+#import "SampleFile.h"
+
+@implementation SampleFile
+
+@end
