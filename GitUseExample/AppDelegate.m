6317f9cb16c2850cd4161355b47a9dddf836fb2e
Mani Murugan <ManiMurugan@manis-mac-mini.local>
2016-01-27 11:37:37 +0530

    SampleTest

--- GitUseExample/AppDelegate.m
+++ GitUseExample/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //SampleTest
     return YES;
 }
 
